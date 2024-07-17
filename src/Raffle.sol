b47f731c6cc39e37dc293e684350a0f11e895f42
CryptoSxk <cryptosxk@gmail.com>
2024-07-17 14:48:16 +0300

    3. Solidity contract layout

---
+++ src/Raffle.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+/**
+ * @author  CryptoSxk
+ * @title   A sample Raffle Contract
+ * @dev     ImplementsChainlink VRFv2
+ * @notice  This contract is for creating a sample raffle
+ */
+
+contract Raffle {
+    uint256 private immutable i_entranceFee;
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+    function enterRaffle() public payable {}
+
+    function pickWinner() public {}
+
+    /* Getter Functtion */
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
+}
